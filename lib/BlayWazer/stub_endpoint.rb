554e5b8e161375a150f5293afc77ea20abcf2b0f
Cliff White <cliff@rmn.com>
2015-09-14 17:31:18 -0500

    got endpoint returning json

--- lib/BlayWazer/stub_endpoint.rb
+++ lib/BlayWazer/stub_endpoint.rb
@@ -1,11 +1,24 @@
 require 'sinatra'
+require 'json'
 
-# GET ''/repos/:owner/:repo/commits'
+PATH_TO_REPONSES = './stub_responses/'
 # todo grab owner and repo in a generic endpoint?
+# todo maybe check for authenticated of not in a helper method
 # todo flatten these commits
 get '/repos/:owner/:repo/commits'  do
   owner = params['owner']
   repo = params['repo']
-  puts owner , repo
+  content_type :json
+  params.to_json
 
+end
+
+def read_from_persistent_storage(fname)
+  file_exists = File.exists? fname
+  file_empty = File.zero?(fname) if file_exists
+  existing = []
+  if file_exists && !file_empty
+    existing = JSON.parse(IO.read(fname))
+  end
+  existing
 end
--- test/test_blaywazer.rb
+++ test/test_blaywazer.rb
@@ -1,12 +1,26 @@
 require 'minitest/autorun'
 require 'blaywazer'
+require 'httmultiparty'
 
 class CommitTest < Minitest::Test
+  include HTTMultiParty
 
-  # Called before every test method runs. Can be used
-  # to set up fixture information.
   def setup
-    # Do nothing
+    @b_uri                        =  'http://localhost:4567/'
+    @options = {:headers => {
+        'User-Agent' => 'CliffWhite',
+        'Origin' => self.class.base_uri,
+        'Connection' => 'keep-alive',
+        'Accept' => 'application/vnd.github.v3+json',
+        'Accept-Encoding' => 'gzip,deflate',
+        'Accept-Language' => 'en-US,en;q=0.8',
+        'Accept-Charset' => 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+        'Content-Type' => 'application/json; charset=UTF-8',
+        'Cache-Control' => 'no-cache'
+
+        }
+    }
+    self.class.base_uri @b_uri
   end
 
   # Called after every test method runs. Can be used to tear
@@ -19,6 +33,17 @@ class CommitTest < Minitest::Test
   # Fake test
   def test_fail
 
-    fail('Not implemented')
+    # fail('Not implemented')
+  end
+
+  def test_no_owner
+    # repos/cliff/blaywazer/commits
+    response = self.class.get('http://localhost:4567/repos/cliff/blaywazer/commits', @options)
+    puts response
+    #
+    # wsmstore_data = JSON.parse(response.body)
+    # wsmstore_data = wsmstore_data['wsmstore']
+    # wsmstore_data
+
   end
 end
